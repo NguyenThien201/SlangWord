ba2f57f6074663914369f7c0afe33c1e471230d1
thiennguyen <k1691nguyenthien@gmail.com>
2020-11-30 22:55:51 +0700

    add menu and color log in console

--- src/MainPkg/main.java
+++ src/MainPkg/main.java
@@ -11,10 +11,22 @@ import java.io.*;
 
 
 public class main {
+
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public static void main(String[] args) {
         HashMap<String, String> capitalCities = new HashMap<String, String>();
         long start = System.currentTimeMillis();
-// some time passes
+
 
         try {
             File myObj = new File("/Users/thiennguyen/HocDi/java_01/slang.txt");
@@ -69,6 +81,33 @@ public class main {
 
         System.out.println("Read list form file...");
         System.out.println(e1.capitalCities.get("CINO_0"));
+        String state = "menu";
+        while (true) {
+            switch (state) {
+                case "menu":
+                    System.out.println("18127270 - Slang word menu");
+                    System.out.println("input the command in \"\" to use");
+                    System.out.println("\"key\" - Search by key    \"mean\" - search by meaning");
+                    System.out.println("\"history\" - Show history    \"mean\" - search by meaning");
+                    Scanner sc = new Scanner(System.in);
+                    System.out.print("Enter the command: ");
+                    state = sc.nextLine();
+                case "key":
+                    Scanner keyScan = new Scanner(System.in);
+                    System.out.print("Enter the key: ");
+                    String searchKey = keyScan.nextLine();
+                    int i = 0;
+//                    System.out.println(searchKey + "_" + i);
+                    while (e1.capitalCities.get(searchKey + "_" + i) != null) {
+                        System.out.println( ">>> " + ANSI_BLUE + searchKey + ANSI_RESET + ANSI_YELLOW + " : " + ANSI_YELLOW + ANSI_BLUE + e1.capitalCities.get(searchKey + "_" + i) + ANSI_RESET );
+                        i+=1;
+                    }
+                    if (i == 0) {
+                        System.out.println(ANSI_RED + "No slang word for \"" + searchKey + "\"" + ANSI_RESET);
+                    }
+            }
+        }
+
 
 
     }
