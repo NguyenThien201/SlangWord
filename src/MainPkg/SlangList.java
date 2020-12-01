ae001dbd23b9cfeb85a4a7f4c314270c472d3562
thiennguyen <k1691nguyenthien@gmail.com>
2020-12-01 23:55:14 +0700

    add search by value function

--- src/MainPkg/SlangList.java
+++ src/MainPkg/SlangList.java
@@ -1,4 +1,5 @@
 package MainPkg;//package src;
+import MainPkg.main;
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
@@ -6,7 +7,148 @@ import java.util.*;
 import java.util.HashMap;
 import java.io.*;
 
- class SlangList implements java.io.Serializable {
-     HashMap<String, String> capitalCities = new HashMap<String, String>();
+class SlangList implements java.io.Serializable {
+
+    HashMap<String, String> slangMap = new HashMap<String, String>();
+    List<String> keySearchHistory = new ArrayList<String>();
+    List<String> valueSearchHistory = new ArrayList<String>();
+
+    public void loadData() {
+        if (!this.importCacheDataFrom("./slangList.ser")) {
+            this.readDataFrom("./slang.txt");
+            this.saveDataTo("./slangList.ser");
+        }
+    }
+
+    public void readDataFrom(String path) {
+        HashMap<String, String> cacheMap = new HashMap<String, String>();
+        try {
+            File myObj = new File(path);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if (data.contains("`")) {
+                    String[] arrOfStr = data.split("`", 2);
+                    int i = 0;
+                    while (cacheMap.get(arrOfStr[i] + "_" + i) != null) {
+                        i += 1;
+                    }
+                    cacheMap.put(arrOfStr[0] + "_" + i, arrOfStr[1]);
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Can not read file from: " + path);
+            e.printStackTrace();
+            return;
+        }
+
+        this.slangMap = cacheMap;
+        System.out.println("import count " + this.slangMap.size());
+        return;
+    }
+
+    public void saveDataTo(String path) {
+        if (!this.slangMap.isEmpty()) {
+            try {
+                FileOutputStream fileOut = new FileOutputStream(path);
+                ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                out.writeObject(this);
+                out.close();
+                fileOut.close();
+            } catch (IOException i) {
+                System.out.println("Can not save to " + path);
+                i.printStackTrace();
+            }
+        } else {
+            System.out.println("There are nothing to save to " + path);
+        }
+    }
+
+    public boolean importCacheDataFrom(String path) {
+        SlangList cacheSlang = new SlangList();
+        try {
+            FileInputStream fileIn = new FileInputStream(path);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            cacheSlang = (SlangList) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            System.out.println("Does not have cache file");
+            return false;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Employee class not found");
+            return false;
+        }
+        this.slangMap = cacheSlang.slangMap;
+        System.out.println("import from cache count  " + this.slangMap.size());
+        return true;
+    }
+
+    /**
+     *
+     * @param value: the search value
+     * @param isAddToHistory: add the search value to history list or not
+     * @return the search key stand for the specific value found in the hash map
+     */
+    public List<String> getWordByValue(String value, boolean isAddToHistory) {
+        if (isAddToHistory) {
+            this.valueSearchHistory.add(value);
+        }
+        return this.getWordByValue(value);
+    }
+
+    /**
+     *
+     * @param value: the search value
+     * @returnthe search key stand for the specific value found in the hash map
+     */
+    public List<String> getWordByValue(String value) {
+        List<String> resultList = new ArrayList<String>();
+        for (String key: this.slangMap.keySet()) {
+            String mapValue = this.slangMap.get(key);
+            if (mapValue.contains(value)) {
+                resultList.add(key);
+            }
+        }
+        return resultList;
+    }
+
+    /**
+     *
+     * @param searchKey: the search key
+     * @param isAddToHistory: add the search key to history list or not
+     * @return the search key stand for the specific value found in the hash map
+     */
+    public List<String> getWordByKey(String searchKey, boolean isAddToHistory) {
+        if (isAddToHistory) {
+            this.keySearchHistory.add(searchKey);
+        }
+       return this.getWordByKey(searchKey);
+    }
+
+    /**
+     *
+     * @param searchKey: the search key
+     * @return the search key stand for the specific value found in the hash map
+     */
+    public List<String> getWordByKey(String searchKey) {
+        List<String> resultList = new ArrayList<String>();
+        int i =0;
+        while (this.slangMap.get(searchKey + "_" + i) != null) {
+            resultList.add(searchKey + "_" + i);
+            i+=1;
+        }
+        return resultList;
+    }
+
+    public void displayAllWordInList(List<String> keyList) {
+        for (String s : keyList) {
+            int lastIndex = s.lastIndexOf("_");
+            String searchKey = s.substring(0, lastIndex);
+            System.out.println( ">>> " + main.ANSI_BLUE + searchKey +  main.ANSI_RESET +  main.ANSI_YELLOW + " : " +  main.ANSI_YELLOW +  main.ANSI_BLUE + this.slangMap.get(s) + main.ANSI_RESET );
+        }
+    }
 
 }
+
--- src/MainPkg/main.java
+++ src/MainPkg/main.java
@@ -4,6 +4,7 @@ import MainPkg.SlangList;
 
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.List;
 import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.HashMap;
 import java.io.*;
@@ -11,7 +12,6 @@ import java.io.*;
 
 
 public class main {
-
 
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
@@ -24,64 +24,15 @@ public class main {
     public static final String ANSI_WHITE = "\u001B[37m";
 
     public static void main(String[] args) {
-        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
         long start = System.currentTimeMillis();
 
 
-        try {
-            File myObj = new File("/Users/thiennguyen/HocDi/java_01/slang.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
+        SlangList slangList = new SlangList();
+        slangList.loadData();
 
-                String data = myReader.nextLine();
-                if (data.contains("`")) {
-                    String[] arrOfStr = data.split("`", 2);
-                    int i = 0;
-                    while (capitalCities.get(arrOfStr[i] + "_" + i) != null) {
-                        i += 1;
-                    }
-                    capitalCities.put(arrOfStr[0] + "_" + i, arrOfStr[1]);
-                }
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
-        SlangList e = new SlangList();
-        System.out.print("Serialized data is saved in /tmp/employee.ser");
-        e.capitalCities = capitalCities;
-        try {
-
-            FileOutputStream fileOut =
-                    new FileOutputStream("/Users/thiennguyen/HocDi/java_01/employee.ser");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(e);
-            out.close();
-            fileOut.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-        }
-        SlangList e1 = null;
-        try {
-            FileInputStream fileIn = new FileInputStream("/Users/thiennguyen/HocDi/java_01/employee.ser");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            e1 = (SlangList) in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-            return;
-        } catch (ClassNotFoundException c) {
-            System.out.println("Employee class not found");
-            c.printStackTrace();
-            return;
-        }
-
-        System.out.println("Read list form file...");
-        System.out.println(e1.capitalCities.get("CINO_0"));
         String state = "menu";
+
         while (true) {
             switch (state) {
                 case "menu":
@@ -89,26 +40,40 @@ public class main {
                     System.out.println("input the command in \"\" to use");
                     System.out.println("\"key\" - Search by key    \"mean\" - search by meaning");
                     System.out.println("\"history\" - Show history    \"mean\" - search by meaning");
+                    System.out.println("At any state press enter to back to the previous screen");
                     Scanner sc = new Scanner(System.in);
                     System.out.print("Enter the command: ");
                     state = sc.nextLine();
+                    break;
                 case "key":
-                    Scanner keyScan = new Scanner(System.in);
-                    System.out.print("Enter the key: ");
-                    String searchKey = keyScan.nextLine();
-                    int i = 0;
-//                    System.out.println(searchKey + "_" + i);
-                    while (e1.capitalCities.get(searchKey + "_" + i) != null) {
-                        System.out.println( ">>> " + ANSI_BLUE + searchKey + ANSI_RESET + ANSI_YELLOW + " : " + ANSI_YELLOW + ANSI_BLUE + e1.capitalCities.get(searchKey + "_" + i) + ANSI_RESET );
-                        i+=1;
-                    }
-                    if (i == 0) {
-                        System.out.println(ANSI_RED + "No slang word for \"" + searchKey + "\"" + ANSI_RESET);
-                    }
+                    performKeySearch(slangList);
+                    break;
+                case "value":
+                    Scanner valueScan = new Scanner(System.in);
+                    System.out.print("Enter the value: ");
+                    String searchKey = valueScan.nextLine();
+//                    int i = 0;
+//                    while (slangList.slangMap.get(searchKey + "_" + i) != null) {
+//                        System.out.println( ">>> " + ANSI_BLUE + searchKey + ANSI_RESET + ANSI_YELLOW + " : " + ANSI_YELLOW + ANSI_BLUE + slangList.slangMap.get(searchKey + "_" + i) + ANSI_RESET );
+//                        i+=1;
+//                    }
+//                    if (i == 0) {
+//                        System.out.println(ANSI_RED + "No slang word for \"" + searchKey + "\"" + ANSI_RESET);
+//                    }
+                    break;
+                default:
+                    System.out.println("haha");
+                    break;
             }
         }
-
-
-
     }
+
+     static void performKeySearch(SlangList slangList) {
+            Scanner keyScan = new Scanner(System.in);
+            System.out.print("Enter the key: ");
+            String searchKey = keyScan.nextLine();
+            List<String> resultList = slangList.getWordByKey(searchKey, true);
+            slangList.displayAllWordInList(resultList);
+    }
+
 }
